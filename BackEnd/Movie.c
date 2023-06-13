e4ad197ad5cfee93bcb6b2ac9a3e4394c4865ede
Hermann <122809747+groupe6CIR1N@users.noreply.github.com>
2023-06-13 23:14:55 +0200

    Update Movie.c
    add comments

--- BackEnd/Movie.c
+++ BackEnd/Movie.c
@@ -4,16 +4,19 @@
 #include <stdio.h>
 #include <string.h>
 
+//Fonction qui permet de créer un movie, avec toute les informations le concernant
 struct Movie* createMovie(char* director, char* name, char* time, char* category){
-    struct Movie* movie = malloc(sizeof(struct Movie));
+    struct Movie* movie = malloc(sizeof(struct Movie));     //On alloue de la mémoire pour stocker notre structure
     if(movie == NULL){
         printf("error malloc");
         return NULL;
     }
-    movie->director = malloc(sizeof(char) * (strlen(director) + 1));
+    //On alloue de la mémoire pour stocker nos données
+    movie->director = malloc(sizeof(char) * (strlen(director) + 1));    
     movie->name = malloc(sizeof(char) * (strlen(name) + 1));
     movie->time = malloc(sizeof(char) * (strlen(time) + 1));
     movie->category = malloc(sizeof(char) * (strlen(category) + 1));
+    //On copie nos données pour pouvoir les stocker
     strcpy(movie->director, director);
     strcpy(movie->name, name);
     strcpy(movie->time, time);
@@ -21,9 +24,11 @@ struct Movie* createMovie(char* director, char* name, char* time, char* category
     return movie;
 }
 
+//Fonction qui permet de supprimer un movie
 void deleteMovie(struct Movie* movie){
-    if(movie->director != NULL){
-        free(movie->director);
+    //Même fonctionnment à chaque fois on change juste de données donc d'emplacement
+    if(movie->director != NULL){    //Si director est différent de NULL
+        free(movie->director);      //On libère la mémoire donc on "supprime" director de notre struct
         movie -> director = NULL;
     }
     if(movie->name != NULL){
@@ -38,5 +43,5 @@ void deleteMovie(struct Movie* movie){
         free(movie->category);
         movie -> category = NULL;
     }
-    free(movie);
+    free(movie);    //On libère la mémoire, pour supprimer la struct movie
 }
